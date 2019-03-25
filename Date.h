82c33bff3649ccf1e81430d1e95fbffc49d0abd3
181174marianadeem <46935461+181174marianadeem@users.noreply.github.com>
2019-03-25 10:45:05 +0500

    Add files via upload

---
+++ Date.h
@@ -0,0 +1,60 @@
+#include"string.h"
+#include<iostream>
+using namespace std;
+
+class Date{
+	static string monthNames[13];
+	int day;
+	int month;
+	int year;
+public:
+	Date();
+	Date(int,int,int);
+	friend ostream &operator<<(ostream & output,const Date&);
+	friend istream &operator>>(istream & input, Date&);
+    bool operator==(const Date&);
+	int operator+(int );
+	void operator=(int);
+	Date&  operator--();
+	Date operator--(int a);
+	int & operator[](int);
+	~Date();
+};
+ostream &operator<<(ostream & output,const Date&d)//extraction
+{
+	/*output<<"date:"<<d.day<<"-"<<d.month<<"-"<<d.year<<endl;
+	return output;*/
+	if(d.month==1)
+		output<<monthNames[1];
+	else if(d.month==2)
+		output<<monthNames[2];
+	else if(d.month==3)
+		output<<monthNames[3];
+	else if (d.month==4)
+		output<<monthNames[4];
+	else if(d.month==5)
+		output<<monthNames[5];
+	else if(d.month==6)
+		output<<monthNames[6];
+	else if (d.month==7)
+		output<<monthNames[7];
+	if(d.month==8)
+		output<<monthNames[8];
+	else if(d.month==9)
+		output<<monthNames[9];
+	else if(d.month==10)
+		output<<monthNames[10];
+	else if (d.month==11)
+		output<<monthNames[11];
+	else
+		output<<monthNames[12];
+	output<<d.day<<","<<d.year;
+	return output;
+
+}
+istream &operator>>(istream & input, Date&d)
+{
+	input>>d.day>>d.month>>d.year;
+	return input;
+}
+static string monthNames[13] = { "", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
---
+++ l181174_imp.cpp
@@ -0,0 +1,129 @@
+#include"string.h"
+#include <iostream>
+#include "Date.h"
+using namespace std;
+
+Date::Date()//default constructor sets the value to 1st jan,2000
+{
+	day=1;
+	month=1;
+	year=2000;
+}
+
+Date::Date(int d,int m,int y)//parameterised constructor
+{
+	if(d<=30&&d>=1&&m<=12&&m>=1&&y<9999&&y>0)
+	{
+		day=d;
+		month=m;
+		year=y;
+	}
+	else
+	{
+		day=1;
+		month=1;
+		year=2000;
+	}
+}
+
+ostream &operator<<(ostream & output,const Date&d)//extraction operator
+{
+	output<<"date:"<<d.day<<"-"<<d.month<<"-"<<d.year<<endl;
+	return output;
+}
+
+istream &operator>>(istream & input, Date&d)//insertion operator
+{
+	input>>d.day>>d.month>>d.year;
+	return input;
+}
+
+//bool Date:: const &operator==(const Date)
+//{
+//
+//}
+bool Date::operator==(const Date&d)//comparision operator
+{
+	if(d.day==day&&d.month==month&&d.year==year)
+		return true;
+	else
+		return false;
+}
+
+int Date::operator+(int d)//addition operator
+{
+	 day=day+d;
+	return day;
+}
+
+//int Date::operator=(int d)
+//{
+//
+//}
+
+//Date:: Date operator=(Date d)
+//{
+//	day=d.day;
+//	month=d.month;
+//	year=d.year;
+//}
+
+void Date::operator=(int d)//assignment operator
+{
+	day=day+d;
+}
+
+//post decrement
+Date&  Date::operator--()
+{
+	if(day==1)
+	{
+		day==30;
+		month--;
+	}
+	else if(month==1)
+	{
+		day=30;
+		month=12;
+		year--;
+	}
+	else
+	day--;
+	return *this;
+}
+
+Date Date::operator--(int a)//pre decrement
+{
+	Date temp(*this);
+	operator--();
+	return temp;
+}
+
+int &Date::operator[](int i)//subscript operator
+{
+	if(i==0)
+	{
+		return day;
+	}
+	else if(i==1)
+	{
+		return month;
+	}
+	else if(i==2)
+	{
+		return year;
+	}
+	else
+	{
+		cout<<"enter valid entry"<<endl;
+		return day;
+	}
+}
+Date::~Date()//destructor
+{
+	day=0;
+	month=0;
+	year=0;
+}
+
+
